b7a9b9519afed6ec16e48774e5789411ce23e8cb
KimJ Manansala <kimjmanansala@gmail.com>
2024-08-08 09:35:57 -0500

    [feat] created first page and implemented chakra library

---
+++ blackjack/pages/blackjacktable/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface IBlackJackTablePage {
+    // Define your interface properties here
+}
+
+const BlackJackTablePage: React.FC<IBlackJackTablePage> = () => {
+    // Implement your component logic here
+
+    return (
+        // JSX code for your component's UI
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default BlackJackTablePage;
