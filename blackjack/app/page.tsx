6083bd14e7467944a1c3d6c992f4a507beff4d7f
KimJ Manansala <kimjmanansala@gmail.com>
2024-08-09 11:41:27 -0500

    [feat] adding test and redirect to home

--- blackjack/app/page.tsx
+++ blackjack/app/page.tsx
@@ -1,16 +1,7 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { Container, useToast } from '@chakra-ui/react';
+import { redirect} from 'next/navigation';
 
-// 1. import `ChakraProvider` component
 
 
 export default function Home() {
-  return (
-
-      <Container maxW='md' bg='blue.600' color='white'>
-                Hi
-        </Container>
-
-  );
+  redirect('/blackjack');
 }
